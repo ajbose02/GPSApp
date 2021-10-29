d7b0b9872f51326c3138071e9ab4ab0e99a01799
ajbose02 <ajbose02@gmail.com>
2021-10-29 07:59:29 -0400

    Initial Commit

---
+++ LocTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocTest
+//
+//  Created by Arjit Bose on 10/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LocTest/LocTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocTestApp.swift
+//  LocTest
+//
+//  Created by Arjit Bose on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LocTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
